5e9c2e5a5888ae7336e591241a1f845368ae0022
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-07-06 16:15:35 -0500

    Add spliceOne to util

--- patches/util.js
+++ patches/util.js
@@ -117,4 +117,11 @@ function once(callback, { preserveReturnValue = false } = kEmptyObject) {
     return result;
   };
 }
-module.exports = { promisify, kEnumerableProperty, customInspectSymbol, isError, join, removeColors, once, kEmptyObject};
+// As of V8 6.6, depending on the size of the array, this is anywhere
+// between 1.5-10x faster than the two-arg version of Array#splice()
+function spliceOne(list, index) {
+  for (; index + 1 < list.length; index++)
+    list[index] = list[index + 1];
+  list.pop();
+}
+module.exports = { promisify, kEnumerableProperty, customInspectSymbol, isError, join, removeColors, once, spliceOne, kEmptyObject};
