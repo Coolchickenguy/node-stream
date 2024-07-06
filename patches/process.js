95589d977ad27e1cdff41268c54410559651dcac
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-07-06 16:11:11 -0500

    Add paramitors to nexttick

--- patches/process.js
+++ patches/process.js
@@ -1,3 +1,3 @@
 // Shim
 require("queue-microtask");
-module.exports = { stdout: "chicken", stderr: "dance", nextTick: queueMicrotask, platform: "linix", env: {} };
+module.exports = { stdout: "chicken", stderr: "dance", nextTick: (value,...params) => queueMicrotask(() => value(...params)), platform: "linix", env: {} };
