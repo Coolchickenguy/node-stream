aa8fcc1eac093e8187a2a65e836f6a9a630b355f
Coolchickenguy <chicken.science.lab@outlook.com>
2024-08-14 14:35:13 -0600

    Clean up files.json and fix utils.parseEnv

--- patches/bindings/util.js
+++ patches/bindings/util.js
@@ -18,7 +18,9 @@ const validate = (types, args) => {
   return true;
 };
 const truthy = (val) => !!val;
-const ni = function(){throw new Error("Not implemented");};
+const ni = function () {
+  throw new Error("Not implemented");
+};
 // Start main implemintation
 const getPromiseDetails = (prom) =>
   getPropsChainSafe(prom, getPromiseDetailsSymbol);
@@ -30,7 +32,10 @@ const isArrayBufferDetached = (val) => truthy(getPropsChainSafe(val, detached));
 const previewEntries = ni;
 const getOwnNonIndexProperties = (obj, code) =>
   validate(["object", "number"], [obj, code]) &&
-  [...Object.getOwnPropertyNames(obj),...Object.getOwnPropertySymbols(obj)].filter(
+  [
+    ...Object.getOwnPropertyNames(obj),
+    ...Object.getOwnPropertySymbols(obj),
+  ].filter(
     (key) =>
       (typeof key == "symbol" || isNaN(key)) &&
       switcher(
@@ -48,25 +53,140 @@ const getOwnNonIndexProperties = (obj, code) =>
           isChecker("writable"),
           isChecker("enumerable"),
           isChecker("configurable"),
-          (_object,Key) => typeof Key != "string",
-          (_object,Key) => typeof Key != "symbol",
+          (_object, Key) => typeof Key != "string",
+          (_object, Key) => typeof Key != "symbol",
         ],
         (...arg) => {
           console.log("err", arg);
           return true;
         }
-      )(obj,key)
+      )(obj, key)
   );
-const getConstructorName = (constructor) => getPropsChainSafe(constructor,"constructor","name");
+const getConstructorName = (constructor) =>
+  getPropsChainSafe(constructor, "constructor", "name");
 // Not needed
 const getExternalValue = ni;
 const sleep = ni;
-const parseEnv = ni;
+// Nodejs's code, just in javascript, not c++
+const parseEnv = function parseEnv(input) {
+  let lines = input;
+
+  // Handle windows newlines "\r\n": remove "\r" and keep only "\n"
+  lines = lines.replace(/\r/g, "");
+
+  let content = lines.trim();
+
+  const store = {};
+
+  while (content.length > 0) {
+    // Skip empty lines and comments
+    if (content[0] === "\n" || /^ *#/m.test(content[0])) {
+      const newline = content.indexOf("\n");
+      if (newline !== -1) {
+        content = content.slice(newline + 1);
+        continue;
+      }
+    }
+
+    // If there is no equal character, then ignore everything
+    const equal = content.indexOf("=");
+    if (equal === -1) {
+      break;
+    }
+
+    let key = content.slice(0, equal).trim();
+    content = content.slice(equal + 1).trim();
+
+    if (key.length === 0) {
+      break;
+    }
+
+    // Remove export prefix from key
+    if (key.startsWith("export ")) {
+      key = key.slice(7);
+    }
+
+    // SAFETY: Content is guaranteed to have at least one character
+    if (content.length === 0) {
+      // In case the last line is a single key without value
+      // Example: KEY= (without a newline at the EOF)
+      store[key] = "";
+      break;
+    }
+
+    // Expand new line if \n it's inside double quotes
+    // Example: EXPAND_NEWLINES = 'expand\nnew\nlines'
+    if (content[0] === '"') {
+      const closingQuote = content.indexOf('"', 1);
+      if (closingQuote !== -1) {
+        let value = content.slice(1, closingQuote);
+        let multiLineValue = value.replace(/\\n/g, "\n");
+
+        store[key] = multiLineValue;
+        content = content.slice(content.indexOf("\n", closingQuote + 1));
+        continue;
+      }
+    }
+
+    // Check if the value is wrapped in quotes, single quotes or backticks
+    if ("'\"`".includes(content[0])) {
+      const closingQuote = content.indexOf(content[0], 1);
+
+      // Check if the closing quote is not found
+      // Example: KEY="value
+      if (closingQuote === -1) {
+        // Check if newline exist. If it does, take the entire line as the value
+        // Example: KEY="value\nKEY2=value2
+        // The value pair should be `"value`
+        const newline = content.indexOf("\n");
+        if (newline !== -1) {
+          const value = content.slice(0, newline);
+          store[key] = value;
+          content = content.slice(newline);
+        }
+      } else {
+        // Example: KEY="value"
+        const value = content.slice(1, closingQuote);
+        store[key] = value;
+        // Select the first newline after the closing quotation mark
+        // since there could be newline characters inside the value.
+        content = content.slice(content.indexOf("\n", closingQuote + 1));
+      }
+    } else {
+      // Regular key value pair.
+      // Example: `KEY=this is value`
+      const newline = content.indexOf("\n");
+
+      let value;
+      if (newline !== -1) {
+        value = content.slice(0, newline);
+        const hashCharacter = value.indexOf("#");
+        // Check if there is a comment in the line
+        // Example: KEY=value # comment
+        // The value pair should be `value`
+        if (hashCharacter !== -1) {
+          value = content.slice(0, hashCharacter);
+        }
+        content = content.slice(newline);
+      } else {
+        // In case the last line is a single key/value pair
+        // Example: KEY=VALUE (without a newline at the EOF)
+        value = content;
+      }
+
+      value = value.trim();
+      store[key] = value;
+    }
+  }
+
+  return store;
+};
 const arrayBufferViewHasBuffer = ni;
 const guessHandleType = ni;
 const isArrayBufferView = (buff) => ArrayBuffer.isView(buff);
 const isTypedArray = (val) => isArrayBufferView(val) && !types.isDataView(val);
-const isUint8Array = (arr) => getPropsChainSafe(arr,"constructor") === Uint8Array;
+const isUint8Array = (arr) =>
+  getPropsChainSafe(arr, "constructor") === Uint8Array;
 module.exports = {
   getPromiseDetails,
   getProxyDetails,
